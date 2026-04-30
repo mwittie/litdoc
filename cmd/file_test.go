7ce982a4ed0f6412e63e75953ade4898e603b2b9
Mike Wittie <mwittie@gmail.com>
2026-04-30 12:38:31 -0600

    Move TestMain and runScript into testscript_test.go

--- cmd/file_test.go
+++ cmd/file_test.go
@@ -1,25 +1,6 @@
 package cmd_test
 
-import (
-	"fmt"
-	"os"
-	"testing"
-
-	"litdoc/cmd"
-
-	"github.com/rogpeppe/go-internal/testscript"
-)
-
-func TestMain(m *testing.M) {
-	testscript.Main(m, map[string]func(){
-		"litdoc": func() {
-			if err := cmd.Execute(); err != nil {
-				fmt.Fprintln(os.Stderr, err)
-				os.Exit(1)
-			}
-		},
-	})
-}
+import "testing"
 
 func TestFileCommand(t *testing.T) {
 	runScript(t, "testdata/script/file.txtar")
@@ -35,12 +16,4 @@ func TestFileCommandNoArgs(t *testing.T) {
 
 func TestFileCommandMissingPath(t *testing.T) {
 	runScript(t, "testdata/script/file_missing_path.txtar")
-}
-
-func runScript(t *testing.T, script string) {
-	t.Helper()
-
-	testscript.Run(t, testscript.Params{
-		Files: []string{script},
-	})
 }
